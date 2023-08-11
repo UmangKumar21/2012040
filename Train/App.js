670e229a92edc13cf10b26e50d432d5f8c16a686
Umang kumar <112698874+UmangKumar21@users.noreply.github.com>
2023-08-11 12:47:19 +0530

    Create App.js

---
+++ Train/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import TrainSched from './components/TrainSched';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <TrainSched />
+    </div>
+  );
+}
+
+export default App;
